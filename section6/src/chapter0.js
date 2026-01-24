3790180dce91b4090c22709ceb05937f91024eaa
Park Seungjae <andrewpark0619@gmail.com>
2026-01-24 12:05:26 +0900

    feat(section6): chapter0 - 자바스크립트의 클래스

---
+++ section6/src/chapter0.js
@@ -0,0 +1,61 @@
+/**
+ * 클래스
+ */
+
+let studentA = {
+  name: "승재",
+  grade: "A",
+  age: 23,
+  study() {
+    console.log("열심히 공부 안 함");
+  },
+  introduce() {
+    console.log("안녕하세요");
+  },
+};
+
+class Student {
+  // 필드
+  name;
+  grade;
+  age;
+
+  // 생성자
+  constructor(name, grade, age) {
+    this.name = name;
+    this.grade = grade;
+    this.age = age;
+  }
+
+  // 메서드
+  study() {
+    console.log("열심히 공부 안 함");
+  }
+
+  introduce() {
+    console.log(`안녕하세요 ${this.name}입니다`);
+  }
+}
+
+class StudentDeveloper extends Student {
+  favoriteSkill;
+
+  constructor(name, grade, age, favoriteSkill) {
+    super(name, grade, age);
+    this.favoriteSkill = favoriteSkill;
+  }
+
+  programming() {
+    console.log(`${this.favoriteSkill}로 프로그래밍 함`);
+  }
+}
+
+// 클래스를 이용해서 만든 객체 -> 인스턴스
+let studentB = new Student("박승재", "A", 23);
+console.log(studentB);
+studentB.study();
+studentB.introduce();
+
+let studentC = new StudentDeveloper("승재", "B", 23, "TypeScript");
+studentC.introduce();
+studentC.programming();
