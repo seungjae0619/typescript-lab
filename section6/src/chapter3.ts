25e78828e99465a7c3cf7138ae3b371fadf45578
Park Seungjae <andrewpark0619@gmail.com>
2026-01-24 12:45:01 +0900

    feat(section6): chapter3 - 인터페이스와 클래스

---
+++ section6/src/chapter3.ts
@@ -0,0 +1,20 @@
+/**
+ * 인터페이스와 클래스
+ */
+
+interface CharacterInterface {
+  name: string;
+  moveSpeed: number;
+  move(): void;
+}
+
+class Character implements CharacterInterface {
+  constructor(
+    public name: string,
+    public moveSpeed: number,
+  ) {}
+
+  move() {
+    console.log(`${this.moveSpeed} 속도로 이동`);
+  }
+}
