03f43d0e628ba4f32b577ed4b69117cd98dd33ae
Park Seungjae <andrewpark0619@gmail.com>
2026-01-26 13:16:25 +0900

    feat(section7): chapter1 - 타입 변수 응용하기

---
+++ section7/src/chapter1.ts
@@ -0,0 +1,39 @@
+/**
+ * 첫 번째 사례
+ */
+
+import { get } from "node:http";
+
+function swap<T, U>(a: T, b: U) {
+  return [b, a];
+}
+
+const [a, b] = swap("1", 2);
+
+/**
+ * 두 번째 사례
+ */
+
+function returnFirstValue<T>(data: [T, ...unknown[]]) {
+  return data[0];
+}
+
+let num = returnFirstValue([0, 1, 2]);
+
+let str = returnFirstValue([1, "hello", "mynameis"]);
+
+/**
+ * 세 번쨰 사례
+ */
+
+function getLength<T extends { length: number }>(data: T) {
+  return data.length;
+}
+
+let var1 = getLength([1, 2, 3]);
+
+let var2 = getLength("12345");
+
+let var3 = getLength({ length: 10 });
+
+// let var4 = getLength(10);
